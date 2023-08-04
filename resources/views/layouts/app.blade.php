cb372d5a980fae6b39dd24a7d044fa53416984ef
Anzar-at-333524458328 <muhammad.anzar@ilsainteractive.com>
2023-08-04 15:01:23 +0500

    tasks selected

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,22 +2,36 @@
 
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Tasks Management System</title>
+    <title>To Do List</title>
+    <!-- Include your CSS and Bootstrap here -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-
 </head>
-
 <body>
-    <script src="{{ asset('js/random-color.js') }}"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const cards = document.querySelectorAll('.selectable-card');
+            const deleteAllButton = document.getElementById('delete-all');
 
-    <div class="top-banner">
-        <h1>Tasks Management System</h1>
-    </div>
+            cards.forEach(card => {
+                card.addEventListener('click', function () {
+                    card.classList.toggle('selected');
+                    updateDeleteAllButtonVisibility();
+                });
+            });
+
+            function updateDeleteAllButtonVisibility() {
+                const selectedCards = document.querySelectorAll('.selectable-card.selected');
+                if (selectedCards.length > 0) {
+                    deleteAllButton.style.display = 'block';
+                } else {
+                    deleteAllButton.style.display = 'none';
+                }
+            }
+        });
+    </script>
     @yield('content')
-
+    <!-- Include your JavaScript here -->
 </body>
-
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,11 +1,21 @@
+<!-- resources/views/tasks/index.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @foreach ($tasks as $task)
-                @include('tasks.task_card', ['task' => $task])
+        <div class="top-banner">
+            <h1>Tasks Management System</h1>
+        </div>
+        <div class="card-container">
+            @foreach($tasks as $task)
+                <div class="card selectable-card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $task->name }}</h5>
+                    </div>
+                </div>
             @endforeach
         </div>
+        <button id="delete-all" class="btn btn-danger delete-all-button">Delete All</button>
     </div>
 @endsection
